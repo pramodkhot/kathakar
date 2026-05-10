8540b0f5e90f3d9a2e7983b7db54b78e84924067
pramodkhot <44355634+pramodkhot@users.noreply.github.com>
2026-05-10 11:31:59 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
+// Top-level build file
 plugins {
-    id("com.android.application") version "8.3.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
-    id("com.google.gms.google-services") version "4.4.2" apply false
-    id("com.google.dagger.hilt.android") version "2.51.1" apply false
+    alias(libs.plugins.android.application)  apply false
+    alias(libs.plugins.kotlin.android)       apply false
+    alias(libs.plugins.google.services)      apply false
+    alias(libs.plugins.hilt)                 apply false
+    alias(libs.plugins.kotlin.kapt)          apply false
 }
